6d33ca0de71fdcdf9375a95648eb853e7f1bacde
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2020-10-27 17:59:40 -0700

    Error message only shows up on failure

--- scripts/notime.py
+++ scripts/notime.py
@@ -5,31 +5,44 @@ from time import sleep
 from subprocess import Popen, DEVNULL, PIPE
 
 STATUS = 'PARENT'
-
+errpipe_read, errpipe_write = (None, None)
 child = None
 
-
 def handle_sigchild(sig_no, stack_frame=None):
+    if STATUS == 'CHILD':
+        if child:
+            write(errpipe_write, child.stderr.read())
+
     if STATUS == 'PARENT':
-        print("Process returned unexpectedly.")
+        error = read(errpipe_read, 1024**3)
+        print(error.decode('utf-8'), file=stderr, end='')
+
     exit(1)
 
 
 if __name__ == '__main__':
+    if len(argv) < 3:
+        print("Usage: %s <time> <command>" % argv[0])
+
     signal(SIGCHLD, handle_sigchild)
 
+    errpipe_read, errpipe_write = pipe()
     pid = fork()
 
     if pid:
+        close(errpipe_write)
         sleep(2)
+        close(errpipe_read)
         exit(0)
 
+    close(errpipe_read)
     STATUS = 'CHILD'
     delay = int(argv[1])
 
     print("Running '%s' for %d seconds as a background process" %
           (" ".join(argv[2:]), delay))
 
-    child = Popen(argv[2:], stdout=DEVNULL)
+    child = Popen(argv[2:], stdout=DEVNULL, stderr=PIPE)
     sleep(delay)
     child.kill()
+    close(errpipe_write)
